9538320a4cadece0ac54239aecbc4173b3ca46ad
ealmulla <elma.almulla@gmail.com>
2023-12-16 23:23:16 +0000

    registration start 1hr

--- src/Pages/Registration.js
+++ src/Pages/Registration.js
@@ -4,12 +4,15 @@ import Header from "../Components/header/Header";
 
 const Registration = () => {
     return(
-        <div>
+        <div className="registration">  
             <Header></Header>
             <h1>registration</h1>
             <div className="registration-box">
                 <form>
-                    
+                    <input className="registration-input" placeholder=" Full name"></input>
+                    <input className="registration-input" placeholder=" Email"></input>
+                    <input className="registration-input" placeholder=" Password"></input>
+                    <input className="registration-input" placeholder=" Confirm password"></input>
                 </form>
             </div>
         </div>
