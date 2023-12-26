135404993f095bb9fd09a9e25152f8503e9beac7
ealmulla <elma.almulla@gmail.com>
2023-12-26 00:58:06 +0000

    mistake

--- backend/db/userUtils.py
+++ backend/db/userUtils.py
@@ -1,11 +1,10 @@
-import json 
-import uuid
+import json
 
 def getUsers():
-    with open("db/userDN.json","r+") as file:
+    with open("db/userDB.json", "r+") as file:
         return json.load(file)
+
 
 def saveUsers(newUsers):
     with open("db/usersDB.json", "w+") as file:
-        file.write( json.dumps(newUsers, indent=4)) 
-        
+        file.write(json.dumps(newUsers, indent=4))
