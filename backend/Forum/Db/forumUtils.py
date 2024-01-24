68a2e4a907556a923e2dc834489913db76628a4f
ealmulla <elma.almulla@gmail.com>
2024-01-24 19:10:56 +0000

    create posts for forum 1hr

--- backend/Forum/Db/forumUtils.py
+++ backend/Forum/Db/forumUtils.py
@@ -4,15 +4,20 @@ import json
 
 #forum class
 class Post(BaseModel):
-    tittle: str
+    title: str
     content: str
     published: bool= True
     rating: Optional[int] = None
 
 def getMyPosts():
-    with open("Forum\Db\MyPostsDb.json",'r+') as file:
+    with open("Forum/Db/myPostsDb.json",'r+') as file:
         return json.load(file)
     
 def saveMyPosts(newPosts):
-    with open("Forum\Db\MyPostsDb.json","w+") as file:
-        return file.write(json.dumps(newPosts, indent=4))
+    with open("Forum/Db/MyPostsDb.json", "w") as file:
+            json.dump(newPosts, file, indent=4)
+
+
+
+
+
--- backend/Forum/forum.py
+++ backend/Forum/forum.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, status
 from pydantic import BaseModel
-
+from random import randrange
 from Forum.Db.forumUtils import getMyPosts, saveMyPosts,Post
 
 router =APIRouter()
@@ -14,6 +14,24 @@ def findPost(id):
             return i
 
 # displaying all posts
-@router.get("/posts",response_model=Post)
+@router.get("/posts")
 def getAllPosts():
     return{"data":myPosts}
+
+@router.post('./createPost',status_code=status.HTTP_201_CREATED)
+def createPost(post:Post):
+    global myPosts
+    postsDict = post.dict()
+    #craeting a random id for the post
+    postsDict["id"] = randrange(0,100000)
+    myPosts.append(postsDict)
+    print("My posts one:",myPosts)
+    saveMyPosts(myPosts)
+    return{"data":postsDict}
+
+
+@router.get("/findpost/{id}")
+def findPostById(id:int):
+    
+    post=findPost(id)
+    return{"data":post}
